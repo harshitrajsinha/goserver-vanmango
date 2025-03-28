f732ef0bb89a5cee2d7136af1479e8fe81ce5892
Harshit <raj.harshitsinha08@gmail.com>
2025-03-29 01:53:27 +0530

    adding authorization

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/harshitrajsinha/van-man-go/driver"
 	"github.com/harshitrajsinha/van-man-go/handler"
+	"github.com/harshitrajsinha/van-man-go/middleware"
 	"github.com/harshitrajsinha/van-man-go/service"
 	"github.com/harshitrajsinha/van-man-go/store"
 	"github.com/joho/godotenv"
@@ -67,20 +68,24 @@ func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", handleHomeRoute).Methods("GET")
+	router.HandleFunc("/login", handler.LoginHandler).Methods("POST")
+
+	protectedRouter := router.PathPrefix("/").Subrouter()
+	protectedRouter.Use(middleware.AuthMiddleware)
 
 	// Routes for Engine
-	router.HandleFunc("/engine/{id}", engineHandler.GetEngineByID).Methods("GET")
-	router.HandleFunc("/engine", engineHandler.GetAllEngine).Methods("GET")
-	router.HandleFunc("/engine", engineHandler.CreateEngine).Methods("POST")
-	router.HandleFunc("/engine/{id}", engineHandler.UpdateEngine).Methods("PUT")
-	router.HandleFunc("/engine/{id}", engineHandler.DeleteEngine).Methods("DELETE")
+	protectedRouter.HandleFunc("/engine/{id}", engineHandler.GetEngineByID).Methods("GET")
+	protectedRouter.HandleFunc("/engine", engineHandler.GetAllEngine).Methods("GET")
+	protectedRouter.HandleFunc("/engine", engineHandler.CreateEngine).Methods("POST")
+	protectedRouter.HandleFunc("/engine/{id}", engineHandler.UpdateEngine).Methods("PUT")
+	protectedRouter.HandleFunc("/engine/{id}", engineHandler.DeleteEngine).Methods("DELETE")
 
 	// Routes for Van
-	router.HandleFunc("/van/{id}", vanHandler.GetVanByID).Methods("GET")
-	router.HandleFunc("/van", vanHandler.GetAllVan).Methods("GET")
-	router.HandleFunc("/van", vanHandler.CreateVan).Methods("POST")
-	router.HandleFunc("/van/{id}", vanHandler.UpdateVan).Methods("PUT")
-	router.HandleFunc("/van/{id}", vanHandler.DeleteVan).Methods("DELETE")
+	protectedRouter.HandleFunc("/van/{id}", vanHandler.GetVanByID).Methods("GET")
+	protectedRouter.HandleFunc("/van", vanHandler.GetAllVan).Methods("GET")
+	protectedRouter.HandleFunc("/van", vanHandler.CreateVan).Methods("POST")
+	protectedRouter.HandleFunc("/van/{id}", vanHandler.UpdateVan).Methods("PUT")
+	protectedRouter.HandleFunc("/van/{id}", vanHandler.DeleteVan).Methods("DELETE")
 
 	port := os.Getenv("PORT")
 	if port == "" {
---
+++ middleware/auth.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt"
+)
+
+var jwtKey = []byte("some_value")
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Authorization header required", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer")
+		claims := &Claims{}
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+		ctx := context.WithValue(r.Context(), "username", claims.Username)
+		next.ServeHTTP(w, r.WithContext(ctx))
+
+	})
+
+}
